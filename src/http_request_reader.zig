283d7cafb758e2eaf40e0d0990dcbe92321fafd5
Gabriel Barbosa Gaspar Veloso <gabrielbgasparv@gmail.com>
2024-02-25 01:55:09 +0000

    Route reading

--- src/http_request_reader.zig
+++ src/http_request_reader.zig
@@ -1,9 +1,20 @@
 const std = @import("std");
 
-const RequestType = enum { get, post, head, put, delete, connect, options, trace, patch };
+const RequestType = enum {
+    get,
+    post,
+    head,
+    put,
+    delete,
+    connect,
+    options,
+    trace,
+    patch,
+};
 
 const RequestInfo = struct {
     request_type: ?RequestType,
+    route: ?[]const u8,
 };
 
 const HttpRequestReader = struct {
@@ -12,6 +23,8 @@ const HttpRequestReader = struct {
     previous_bytes: std.ArrayList(u8),
     cursor_position: u32,
     strings_to_request_types: std.StringHashMap(RequestType),
+    has_just_read_http_method: bool,
+    has_just_begun: bool,
 
     const Self = @This();
 
@@ -21,6 +34,7 @@ const HttpRequestReader = struct {
         const request_info = try allocator.create(RequestInfo);
 
         request_info.request_type = null;
+        request_info.route = null;
 
         const previous_bytes = std.ArrayList(u8).init(allocator);
 
@@ -33,6 +47,8 @@ const HttpRequestReader = struct {
             .request_info = request_info,
             .previous_bytes = previous_bytes,
             .strings_to_request_types = strings_to_request_types,
+            .has_just_read_http_method = false,
+            .has_just_begun = true,
             .cursor_position = 0,
         };
     }
@@ -44,18 +60,53 @@ const HttpRequestReader = struct {
     }
 
     pub fn readNextBytes(self: *Self, next_bytes: []const u8) !void {
-        for (next_bytes) |next_byte| {
-            if (next_byte == ' ')
-                break;
+        if (self.hasJustReadHttpMethod()) {
+            self.clearPreviousBytes();
 
-            try self.previous_bytes.append(next_byte);
+            for (next_bytes) |next_byte| {
+                if (next_byte == ' ')
+                    break;
+
+                try self.previous_bytes.append(next_byte);
+            }
+
+            self.request_info.route = self.previous_bytes.items;
+        } else if (self.hasJustBegun()) {
+            for (next_bytes) |next_byte| {
+                if (next_byte == ' ')
+                    break;
+
+                try self.previous_bytes.append(next_byte);
+            }
+
+            const string = self.previous_bytes.items;
+
+            if (self.strings_to_request_types.get(string)) |item| {
+                self.request_info.request_type = item;
+            }
         }
+    }
 
-        const string = self.previous_bytes.items;
+    pub fn clearPreviousBytes(self: *Self) void {
+        self.previous_bytes.deinit();
 
-        if (self.strings_to_request_types.get(string)) |item| {
-            self.request_info.request_type = item;
-        }
+        self.previous_bytes = std.ArrayList(u8).init(self.allocator);
+    }
+
+    pub fn setHasJustReadHttpMethod(self: *Self, has_just_read_http_method: bool) void {
+        self.has_just_read_http_method = has_just_read_http_method;
+    }
+
+    pub fn hasJustReadHttpMethod(self: *Self) bool {
+        return self.has_just_read_http_method;
+    }
+
+    pub fn setHasJustBegun(self: *Self, has_just_begun: bool) void {
+        self.has_just_begun = has_just_begun;
+    }
+
+    pub fn hasJustBegun(self: *Self) bool {
+        return self.has_just_begun;
     }
 };
 
@@ -130,4 +181,21 @@ fn assertVerbIsKnown(tuple: HttpMethodTuple) !void {
     const request_info = http_request_reader.request_info;
 
     try std.testing.expectEqual(tuple[1], request_info.request_type);
+}
+
+test "HttpRequestReader reports correct route" {
+    const request: []const u8 = "/a/b/c";
+
+    const allocator = std.testing.allocator;
+
+    var http_request_reader = try HttpRequestReader.init(allocator);
+    defer http_request_reader.deinit();
+
+    http_request_reader.setHasJustReadHttpMethod(true);
+
+    try http_request_reader.readNextBytes(request);
+
+    for (request, http_request_reader.request_info.route.?) |c1, c2| {
+        try std.testing.expectEqual(c1, c2);
+    }
 }
