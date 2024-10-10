7dba46145ab4cfe7d15299d21c3590e67e30578e
Gabriel Barbosa Gaspar Veloso <gabrielbgasparv@gmail.com>
2024-10-10 10:06:33 +0100

    Different implementation for DelimiterReader

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+pub const main = @import("main.zig").main;
+
 test "all tests" {
     _ = @import("./http_one_dot_one_request_reader.zig");
     _ = @import("./str/string_delimiter_reader.zig");
--- src/str/string_delimiter_reader.zig
+++ src/str/string_delimiter_reader.zig
@@ -1,116 +1,68 @@
 const std = @import("std");
 
-pub const StringDelimiterReader = struct {
-    const Self = @This();
+pub fn DelimiterReader(comptime T: type) type {
+    return struct {
+        delimiter: []const T,
+        delimiter_index: usize,
+        current_match_index: ?usize,
+        read_char_count: usize,
 
-    pub fn init(_: std.mem.Allocator) !Self {
-        return Self{};
-    }
+        const Self = @This();
 
-    pub fn deinit(_: *Self) void {}
-
-    pub fn readBufferIntoArrayListUntilDelimiter(self: *Self, comptime T: type, buffer: []const T, array_list: *std.ArrayList(T), delimiter: []const T) !void {
-        if (delimiter.len == 0)
-            return error.EmptyDelimiter;
-
-        if (self.findDelimiterIndex(T, buffer, delimiter)) |i| {
-            try array_list.appendSlice(buffer[0..i]);
-        } else {
-            try array_list.appendSlice(buffer);
+        pub fn init(delimiter: []const T, _: std.mem.Allocator) !Self {
+            return Self{
+                .delimiter = delimiter,
+                .delimiter_index = 0,
+                .current_match_index = null,
+                .read_char_count = 0,
+            };
         }
-    }
 
-    fn findDelimiterIndex(_: *Self, comptime T: type, buffer: []const T, delimiter: []const T) ?usize {
-        var i: usize = 0;
-        var delimiter_i: usize = 0;
-        var match_start_index: ?usize = null;
+        pub fn readNextItems(self: *Self, buffer: []const T) ?usize {
+            var buffer_index: usize = 0;
 
-        while (i < buffer.len and delimiter_i < delimiter.len) {
-            if (buffer[i] == delimiter[delimiter_i]) {
-                match_start_index = match_start_index orelse i;
-                delimiter_i += 1;
-            } else {
-                delimiter_i = 0;
-                match_start_index = null;
+            while (buffer_index < buffer.len) {
+                if (buffer[buffer_index] == self.delimiter[self.delimiter_index]) {
+                    self.delimiter_index += 1;
+                    self.current_match_index = self.current_match_index orelse self.read_char_count;
+                } else {
+                    self.current_match_index = null;
+                    self.delimiter_index = 0;
+                }
+
+                if (self.delimiter_index == self.delimiter.len) {
+                    return self.current_match_index;
+                }
+
+                buffer_index += 1;
+                self.read_char_count += 1;
             }
 
-            i += 1;
+            return null;
         }
 
-        if (delimiter_i < delimiter.len) {
-            match_start_index = null;
-        }
-
-        return match_start_index;
-    }
-};
-
-test "StringDelimiterReader returns an error if an empty delimiter is specified" {
-    const allocator = std.testing.allocator;
-
-    var string_delimiter_reader = try StringDelimiterReader.init(allocator);
-    defer string_delimiter_reader.deinit();
-
-    var array_list = std.ArrayList(u8).init(allocator);
-    defer array_list.deinit();
-
-    const err = string_delimiter_reader.readBufferIntoArrayListUntilDelimiter(u8, "ABC", &array_list, "");
-
-    try std.testing.expectEqual(error.EmptyDelimiter, err);
+        pub fn deinit(_: *Self) void {}
+    };
 }
 
-test "StringDelimiterReader writes a \"AB\" out of \"ABC\" with single-character delimiter \"C\"" {
+test "DelimiterReader returns null on .getMatchIndex() if full delimiter was not yet found" {
     const allocator = std.testing.allocator;
 
-    var string_delimiter_reader = try StringDelimiterReader.init(allocator);
-    defer string_delimiter_reader.deinit();
+    var delimiter_reader = try DelimiterReader(u8).init("C", allocator);
+    defer delimiter_reader.deinit();
 
-    var array_list = std.ArrayList(u8).init(allocator);
-    defer array_list.deinit();
+    const result = delimiter_reader.readNextItems("");
 
-    try string_delimiter_reader.readBufferIntoArrayListUntilDelimiter(u8, "ABC", &array_list, "C");
-
-    try std.testing.expectEqualStrings("AB", array_list.items);
+    try std.testing.expectEqual(null, result);
 }
 
-test "StringDelimiterReader writes \"Test\" out of \"Test phrase\" with space delimiter" {
+test "DelimiterReader returns 2 when attempting to get \"C\" from \"ABC\"" {
     const allocator = std.testing.allocator;
 
-    var string_delimiter_reader = try StringDelimiterReader.init(allocator);
-    defer string_delimiter_reader.deinit();
+    var delimiter_reader = try DelimiterReader(u8).init("C", allocator);
+    defer delimiter_reader.deinit();
 
-    var array_list = std.ArrayList(u8).init(allocator);
-    defer array_list.deinit();
+    const result = delimiter_reader.readNextItems("ABC");
 
-    try string_delimiter_reader.readBufferIntoArrayListUntilDelimiter(u8, "Test phrase", &array_list, " ");
-
-    try std.testing.expectEqualStrings("Test", array_list.items);
-}
-
-test "StringDelimiterReader does not write \"Test p\", but \"Test phrase\", out of \"Test phrase\" with \"hrae\" delimiter" {
-    const allocator = std.testing.allocator;
-
-    var string_delimiter_reader = try StringDelimiterReader.init(allocator);
-    defer string_delimiter_reader.deinit();
-
-    var array_list = std.ArrayList(u8).init(allocator);
-    defer array_list.deinit();
-
-    try string_delimiter_reader.readBufferIntoArrayListUntilDelimiter(u8, "Test phrase", &array_list, "hrae");
-
-    try std.testing.expectEqualStrings("Test phrase", array_list.items);
-}
-
-test "StringDelimiterReader writes whole buffer even if delimiter is longer than buffer" {
-    const allocator = std.testing.allocator;
-
-    var string_delimiter_reader = try StringDelimiterReader.init(allocator);
-    defer string_delimiter_reader.deinit();
-
-    var array_list = std.ArrayList(u8).init(allocator);
-    defer array_list.deinit();
-
-    try string_delimiter_reader.readBufferIntoArrayListUntilDelimiter(u8, "T", &array_list, "Tea");
-
-    try std.testing.expectEqualStrings("T", array_list.items);
+    try std.testing.expectEqual(2, result);
 }
